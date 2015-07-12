c12c6e81555c4eb787aaf9f9322e622d7bfa8105
Daniel Libanori <daniellibanori@gmail.com>
2015-07-12 05:28:07 -0300

    Add controllers and views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,28 @@
 class MessagesController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.create(message_params)
+    render :new unless @message.save
   end
 
   def show
+    @message = Message.find_by(secret: params[:secret])
+    render :not_found unless @message
   end
 
   def update
+    @message = Message.find_by(secret: params[:secret])
+    @message.destroy
+
+  end
+
+  private
+  def message_params
+    params.permit(:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'messages/new'
-
-  get 'messages/show'
-
   get '/' => 'messages#new'
   post '/' => 'messages#create'
-  get '/:uuid' => 'messages#show'
-  patch '/:uuid' => 'messages#update'
+  get '/:secret' => 'messages#show'
+  patch '/:secret' => 'messages#update'
 end
