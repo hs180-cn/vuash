74692a24f0a769a599c0723e145475152e2177cb
Daniel Libanori <daniellibanori@gmail.com>
2015-07-15 00:34:15 -0300

    Extend controller spec

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,16 +2,25 @@ require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
 
-  describe "GET #new" do
-    it "returns http success" do
+  describe 'GET #new' do
+    it 'returns http success' do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
+  describe 'POST #create' do
+    it 'returns http success' do
+      post :create, message: { body: 'Some text' }, secret: 'secret'
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+
+  describe 'GET #show' do
+    before { allow(Message).to receive(:find) { double }}
+
+    it 'returns http success' do
+      get :show, id: 1
       expect(response).to have_http_status(:success)
     end
   end
